01bb5371442216b0173e983bd3716e7c614155ac
weididev <snehamoydutta11@gmail.com>
2026-04-21 20:17:03 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shiftmanager.pro',
+  appName: 'Shift Manager Pro',
+  webDir: 'dist'
+};
+
+export default config;
