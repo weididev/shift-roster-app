a610df0b33fd3477f1fcaa93b4f69e8fa562e09d
weididev <snehamoydutta11@gmail.com>
2026-04-30 18:09:49 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,9 @@
-{
-  "appId": "com.shiftmanager.pro",
-  "appName": "Shift Manager Pro",
-  "webDir": "dist",
-  "bundledWebRuntime": false
-}
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shiftmanager.pro',
+  appName: 'Shift Manager Pro',
+  webDir: 'dist'
+};
+
+export default config;
